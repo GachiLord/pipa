d10ae5258bc9a5af958efd9a9030d92434dfd7d4
GachiLord <name504172@gmail.com>
2026-02-28 21:59:42 +0500

    add negative tests

---
+++ tests/negative.rs
@@ -0,0 +1,44 @@
+mod utils;
+
+use std::fs::{read_to_string};
+use pipa::ir::gen_ir;
+use pipa::syntax::{ast, TokenType};
+use pipa::error::{CompileError, ErrorReason};
+use utils::err_reason;
+
+fn test_str(code: &str) -> Result<(), CompileError> {
+    let nodes = ast(code)?;
+
+    let _ = gen_ir(code, nodes)?;
+    Ok(())
+}
+
+fn test_file(filename: &str) -> Result<(), CompileError> {
+    let code = read_to_string(filename).unwrap();
+
+    test_str(&code)?;
+    Ok(())
+}
+
+
+#[test]
+fn undefined_scope() {
+    assert_eq!(err_reason(test_file("negative_examples/undefined_scope.pipa")), ErrorReason::UndefinedVar { name: "_name".into() });
+}
+
+#[test]
+fn undefined_scope_single() {
+
+    assert_eq!(err_reason(test_file("negative_examples/undefined_scope_single.pipa")), ErrorReason::UndefinedVar { name: "_".into() });
+}
+
+#[test]
+fn undefined_scope_with_range() {
+    assert_eq!(err_reason(test_file("negative_examples/undefined_scope_with_range.pipa")), ErrorReason::UndefinedVar { name: "_name".into() });
+}
+
+#[test]
+fn broken_string() {
+    assert_eq!(err_reason(test_file("negative_examples/broken_string.pipa")), ErrorReason::SyntaxError { expected: vec![TokenType::Quote] }); 
+}
+
--- tests/positive.rs
+++ tests/positive.rs
@@ -42,8 +42,6 @@ fn test_str(f: &mut impl Write, filename: &str, code: &str, output: &str) {
 }
 
 
-// positive
-
 #[test]
 fn positive_snippets() {
     let mut stdout = stdout().lock();
@@ -64,36 +62,3 @@ fn positive_snippets() {
     }
     
 }
-
-// negative
-/*
-fn test_file(filename: &str) -> Result<(), CompileError> {
-    let code = read_to_string(filename).unwrap();
-
-    test_str(&code)?;
-    Ok(())
-}
-
-
-#[test]
-fn undefined_scope() {
-    assert_eq!(err_reason(test_file("negative_examples/undefined_scope.pipa")), ErrorReason::UndefinedVar { name: "_name".into() });
-}
-
-#[test]
-fn undefined_scope_single() {
-
-    assert_eq!(err_reason(test_file("negative_examples/undefined_scope_single.pipa")), ErrorReason::UndefinedVar { name: "_".into() });
-}
-
-#[test]
-fn undefined_scope_with_range() {
-    assert_eq!(err_reason(test_file("negative_examples/undefined_scope_with_range.pipa")), ErrorReason::UndefinedVar { name: "_name".into() });
-}
-
-#[test]
-fn broken_string() {
-    assert_eq!(err_reason(test_file("negative_examples/broken_string.pipa")), ErrorReason::SyntaxError { expected: vec![TokenType::Quote] }); 
-}
-
-*/
